5e35a28d0182dd58d676a0f956c26516213f97e2
Cahebe Correa <cahebe@gmail.com>
2022-02-08 13:05:03 -0300

    Projeto criado

---
+++ src/application/AulaGit.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AulaGit {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Que horas são agora? ");
+		int horas = sc.nextInt();
+		sc.nextLine();
+		
+
+		
+		
+		
+		sc.close();
+
+	}
+
+}
